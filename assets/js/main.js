9a820cf1c031eae453cdf00298f0bf695372d19a
Korzh Aleksey <korzh.alexey@yahoo.com>
2020-12-08 22:59:56 +0200

    check

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+
+//     $('.header-navbar__hamburger').on('click', function() {
+//         $(this).toggleClass('active');
+//         $('.header-navbar__list').toggleClass('active');
+//     });
+
+// });
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
